5f11fc80807f345c5308749cc419e8f506e9671b
Kaival <87368466+Kaival1212@users.noreply.github.com>
2024-11-10 02:06:06 +0000

    Update join-wait-list-user-mail.blade.php

--- resources/views/mail/join-wait-list-user-mail.blade.php
+++ resources/views/mail/join-wait-list-user-mail.blade.php
@@ -1,29 +1,30 @@
 @component('mail::message')
-<x-mail::panel>
-![LinkUP Logo]
-</x-mail::panel>
+# LinkUP
 
 # 🎉 Welcome to the LinkUP Waitlist! 🎉
 
 Hi {{ $waitListUser->email }},
 
 Thank you for joining the waitlist for **LinkUP**! 🚀
+
 We're excited to bring you a brand-new social media experience that will revolutionize the way you connect with others.
 
 We'll notify you as soon as a spot opens up for our beta program, so stay tuned! 📩
 
-## Got Questions or Feedback? 💬
+@component('mail::panel')
+### Got Questions or Feedback? 💬
 
 Feel free to reply to this email with any questions or feedback you might have. We're always looking for ways to improve and your suggestions mean a lot to us! 😊
+@endcomponent
 
-## Join Our Team! 🙌
+@component('mail::panel')
+### Join Our Team! 🙌
 
 If you want to be part of the LinkUP journey, send us your CV and we'll be in touch! 💼
+@endcomponent
 
 Thanks, and we can't wait to have you on board! 💙
 
-
-
-Thanks,<br>
+Best regards,<br>
 {{ config('app.name') }} Team
 @endcomponent
